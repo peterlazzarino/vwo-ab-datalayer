774a4b1ba53a55be35ed4fb67869899a8bec527d
peterlazzarino <peter.lazzarino@gmail.com>
2018-09-26 15:21:59 -0400

    Update datalayer to work with async code

--- src/Datalayer.ts
+++ src/Datalayer.ts
@@ -5,14 +5,33 @@ import * as indexOf from "array-index-of";
 declare var VWO: any;
 
 const canUseDOM = typeof window !== "undefined";
-const vwoReady = typeof VWO !== "undefined";
+const vwoReady = typeof VWO !== "undefined" && VWO.v;
 
 const currentCampaigns = [];
 const campaignListeners = [];
 
+const checkForVWO = () => {
+    return typeof VWO !== "undefined" && VWO.v; 
+}
+
+const activateVWO = () => {
+    VWO.onVariationApplied((x) => pushUpdate(x));
+}
+
 (() => {
-    if (canUseDOM && vwoReady) {
-        VWO.onVariationApplied((x) => pushUpdate(x));
+    if (!canUseDOM){
+        return;
+    }
+    if (checkForVWO()) {
+        activateVWO();
+    }
+    else{        
+        var interval = setInterval(function(){
+            if (checkForVWO()) {
+                clearInterval(interval);
+                activateVWO();
+            }
+        }, 50);
     }
 })();
 
